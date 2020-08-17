d49286e511a8d73d2e435db9f15c7218abf5fbc8
unknown <engel199702@hotmail.com>
2020-08-17 08:56:21 -0600

    1. Getting Started

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import GettingStartedForm from './forms/GettingStartedForm/GettingStartedForm';
+
+function App() {
+  return (
+    <div className="App">
+      <GettingStartedForm/>
+    </div>
+  );
+}
+
+export default App;
